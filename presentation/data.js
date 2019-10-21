51805f5ad7c7b071f92fd09da39953d9766ce052
Riki Fridrich <riki@fczbkk.com>
2019-10-21 07:07:45 +0200

    simplify data

--- presentation/data.js
+++ presentation/data.js
@@ -3,112 +3,96 @@ export const products = [
     'id': 'beanie-hat',
     'title': 'Simple hat',
     'image': 'childrens-black-beanie.jpg',
-    'price': 100,
-    'availability': 10
+    'price': 100
   },
   {
     'id': 'boho-earrings',
     'title': 'Earrings',
     'image': 'boho-earrings.jpg',
-    'price': 450,
-    'availability': 0
+    'price': 450
   },
   {
     'id': 'brown-purse-with-shoulder-strap',
     'title': 'Purse with shoulder strap',
     'image': 'brown-purse-with-shoulder-strap.jpg',
-    'price': 1500,
-    'availability': 5
+    'price': 1500
   },
   {
     'id': 'beard-oil-bottles',
     'title': 'Beard oil',
     'image': 'beard-oil-bottles.jpg',
-    'price': 500,
-    'availability': 2
+    'price': 500
   },
   {
     'id': 'galaxy-earrings',
     'title': 'Galaxy earrings',
     'image': 'galaxy-earrings.jpg',
-    'price': 1200,
-    'availability': 7
+    'price': 1200
   },
   {
     'id': 'gold-zipper-on-black-fashion-backpack',
     'title': 'Fashion backpack',
     'image': 'gold-zipper-on-black-fashion-backpack.jpg',
-    'price': 2000,
-    'availability': 20
+    'price': 2000
   },
   {
     'id': 'kids-fashion-shorts',
     'title': 'Shorts for kids',
     'image': 'kids-fashion-shorts.jpg',
-    'price': 350,
-    'availability': 70
+    'price': 350
   },
   {
     'id': 'navy-blue-and-white-left-shoe',
     'title': 'Navy blue shoes',
     'image': 'navy-blue-and-white-left-shoe.jpg',
-    'price': 1200,
-    'availability': 50
+    'price': 1200
   },
   {
     'id': 'pair-of-navy-blue-skate-shoes',
     'title': 'Navy blue skate shoes',
     'image': 'pair-of-navy-blue-skate-shoes.jpg',
-    'price': 900,
-    'availability': 15
+    'price': 900
   },
   {
     'id': 'purple-gemstone-necklace',
     'title': 'Gemstone necklace',
     'image': 'purple-gemstone-necklace.jpg',
-    'price': 1700,
-    'availability': 3
+    'price': 1700
   },
   {
     'id': 'red-LED-shoes',
     'title': 'Red LED shoes',
     'image': 'red-LED-shoes.jpg',
-    'price': 1000,
-    'availability': 50
+    'price': 1000
   },
   {
     'id': 'bathing-suit',
     'title': 'Bathing suit',
     'image': 'summer-print-bathing-suit.jpg',
-    'price': 800,
-    'availability': 50
+    'price': 800
   },
   {
     'id': 'swim-shorts',
     'title': 'Swim shorts',
     'image': 'swim-shorts.jpg',
-    'price': 800,
-    'availability': 0
+    'price': 800
   },
   {
     'id': 'tube-of-lip-balm',
     'title': 'Lip balm',
     'image': 'tube-of-lip-balm.jpg',
-    'price': 70,
-    'availability': 0
+    'price': 70
   },
   {
     'id': 'black-sneakers-with-white-sole',
     'title': 'Black sneakers with white sole',
     'image': 'black-sneakers-with-white-sole.jpg',
-    'price': 800,
-    'availability': 50
+    'price': 800
   },
   {
     'id': 't-shirt',
     'title': 'Simple t-shirt',
     'image': 'blue-t-shirt.jpg',
-    'price': 300,
-    'availability': 500
+    'price': 300
   }
 ]
